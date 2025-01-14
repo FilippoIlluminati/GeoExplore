cb44e6a999e315e1b9406cbdffb66d499c4dcb3f
Michele <michel.falistocco@studenti.unicam.it>
2025-01-14 22:13:13 +0100

    Implementation database h2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("java")
     id("application")
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "org.geoexplore"
@@ -11,42 +13,26 @@ repositories {
 }
 
 dependencies {
-    implementation("com.google.code.gson:gson:2.8.9")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation("org.springframework.boot:spring-boot-starter-web") // Aggiunto per abilitare il server web
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.h2database:h2")
 
-    // Dipendenze JavaFX
+    // Altre dipendenze esistenti
+    implementation("com.google.code.gson:gson:2.8.9")
     implementation("org.openjfx:javafx-controls:23.0.1")
     implementation("org.openjfx:javafx-fxml:23.0.1")
-    implementation("org.openjfx:javafx-web:23.0.1")  // Aggiungi il modulo javafx-web
-    implementation("org.json:json:20210307") // Aggiungi questa linea
+    implementation("org.openjfx:javafx-web:23.0.1")
+    implementation("org.json:json:20210307")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 application {
-    mainClass.set("org.geoexplore.App")
+    mainClass.set("org.geoexplore.GeoExploreApplication")
 }
-
 
 tasks.test {
     useJUnitPlatform()
-}
-
-// Configura il modulo JavaFX
-java {
-    modularity.inferModulePath.set(true)
-}
-
-tasks.withType<JavaExec> {
-    jvmArgs = listOf(
-            "--module-path", "C:/Programmi/javafx-sdk-23.0.1/lib",
-            "--add-modules", "javafx.controls,javafx.fxml,javafx.web"
-    )
-}
-
-tasks.withType<JavaCompile> {
-    options.compilerArgs = listOf(
-            "--module-path", "C:/Programmi/javafx-sdk-23.0.1/lib",
-            "--add-modules", "javafx.controls,javafx.fxml,javafx.web"
-    )
 }
 
--- app/src/main/java/org/geoexplore/GeoExploreApplication.java
+++ app/src/main/java/org/geoexplore/GeoExploreApplication.java
@@ -1,4 +1,11 @@
-package main.java.org.geoexplore;
+package org.geoexplore;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class GeoExploreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GeoExploreApplication.class, args);
+    }
 }
